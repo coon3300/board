ee248ec5204f7c6ffe64498595bf43ed005fcbfe
coon3300 <coon3300@gmail.com>
2024-07-17 17:49:19 +0900

    BoardWeb 추가

---
+++ BoardWeb/src/test/java/com/yedam/common/AppTest.java
@@ -0,0 +1,21 @@
+package com.yedam.common;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.yedam.mapper.StudentMapper;
+
+public class AppTest {
+	
+	public static void main(String[] args) {
+		SqlSession sqlSession = DataSource.getInstance().openSession();
+		
+		StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+		
+		mapper.studentList();
+		
+		mapper.studentList().forEach(student -> {
+			System.out.println(student);
+		});
+		
+	}
+}
